59b626265defbe4e3c07d296e33e43959042ed92
Трофимов Дитрий Игоревич <trofimovdi@mts.ru>
2024-07-06 02:53:38 +0300

    wip

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,21 @@
 // app/page.tsx
 import { query } from '../db';
 
-export default async function Home() {
+async function fetchDiameters() {
   let diameters = [];
 
   try {
-    const res = await query('SELECT * FROM metric.diameters');
+    const res = await query('SELECT * FROM metric.diameters ORDER BY id ASC ');
     diameters = res.rows;
   } catch (err) {
     console.error('Error fetching diameters:', err);
   }
+
+  return diameters;
+}
+
+export default async function Home() {
+  const diameters = await fetchDiameters();
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
